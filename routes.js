31ba5b6c9c88819c1194145991b6eb4b5772c892
Linsy <Linsy.Joyner@gmail.com>
2017-05-29 16:27:38 -0400

    storing user in mongodb

--- routes.js
+++ routes.js
@@ -4,3 +4,35 @@ const express = require('express');
 const User = require('./user');
 
 const router = express.Router();
+
+router.post('/users', function(req, res, next) {
+  if (req.body.email &&
+    req.body.username &&
+    req.body.password) {
+      const userData = {
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password,
+      };
+
+      User.create(userData, function (error, user) {
+        if (error) {
+          return next(error);
+        } else {
+          res.json({ body: user });
+        }
+      });
+
+      if (req.body.password !== req.body.confirmPassword) {
+        const err = new Error('Passwords do not match.')
+        err.status = 400;
+        return next(err);
+      }
+    } else {
+      const err = new Error('All fields required.');
+      err.status = 400;
+      return next(err);
+    }
+});
+
+module.exports = router;
