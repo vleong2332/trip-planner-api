aeb3307880938ba68edbb754eb39b52173d94a21
Linsy <Linsy.Joyner@gmail.com>
2017-06-10 15:53:37 -0400

    test for root path

--- tests.js
+++ tests.js
@@ -4,7 +4,7 @@ const app = require('./app');
 describe('Requests to the root path', function() {
   it('Returns 200 status code', function(done) {
     request(app)
-      .get('/')
+      .get('/trip-planner/')
       .expect(200, done)
-  })
+  });
 });
